1b33d904a726adbc279afbb72a773a4318175183
john11000 <86936709+john11000@users.noreply.github.com>
2023-09-05 07:21:41 +0000

    Update guardar_db_cloud.py

--- Scripts/guardar_db_cloud.py
+++ Scripts/guardar_db_cloud.py
@@ -1,14 +1,9 @@
 import os
 import json
-import pymongo
 import requests
  # Conexion a mongodb
-client = pymongo.MongoClient('mongodb+srv://anderson:anderson@cluster0.hykao.mongodb.net')
 
 # base de datos para desarrollo
-db_dev = client.animes
-collection_dev = db_dev.animes
-
 carpetas_excluidas = ['My Music', 'My Pictures', 'My Videos', 'desktop.ini']
 
 def directorio_a_json(directorio):
@@ -32,11 +27,14 @@ estructura_json = directorio_a_json(directorio_base)
 #     json.dump(estructura_json, archivo_json, indent=4)
 #     collection_dev.insert_one([*estructura_json])
 # Serialize the Python object to JSON
-json_data = json.dumps({"animes": estructura_json})
-
+json_data = json.dumps({"animes":estructura_json})
 # Send the JSON data in the request
-response = requests.post("https://flask-server2.vercel.app/saveToDb", json=json_data, headers={"Content-Type": "application/json"})
+response = requests.post("https://flask-server2.vercel.app/saveToDb", data=json_data, headers={"Content-Type": "application/json"})
 
 print(response)
    # collection_dev.insert_one(video_to_save)
 print("Estructura JSON generada y guardada en estructura.json")
+
+
+# with open("db1.json", "w") as f:
+#     json.dump({"animes":estructura_json}, f)
